e80e07d5efc65ae6aab0a74c9804f084aaed253d
Carson Lenzinger <02carson.lenzinger@gulllakecs.org>
2021-02-08 12:30:45 -0500

    Edited the MainActivity.java File and Added a Command to the Button

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -1,14 +1,36 @@
+/**
+ * The package in this file has to match the AndroidManifest.xml package found in the manifests
+ * folder and if this files package is different then change this files package not the
+ * AndroidManifest package
+ */
+
 package com.example.justjava;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+/**
+ * This app displays an order form to order coffee.
+ */
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display(1);
+    }
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
 }
+
