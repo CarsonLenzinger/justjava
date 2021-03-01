263a2d4a784c3bf8fa412f55f5d4e971b94050a0
Carson Lenzinger <02carson.lenzinger@gulllakecs.org>
2021-03-01 12:35:27 -0500

    Updated increment and decrement methods

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -16,30 +16,44 @@ import android.widget.TextView;
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
+
+    int quantity = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void increment(View view) {
+        quantity++;
+        display(quantity);
+    }
+
+
+
+
+public void decrement(View view){
+        if(quantity>0) {
+            quantity--;
+            display(quantity);
+        }
+}
+
+
+
+
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int coffeesOrdered = 3;
-        display(coffeesOrdered);
+        int coffeesOrdered = quantity;
+        display(quantity);
         displayPrice(coffeesOrdered * 5);
     }
 
-    public void increment(View view) {
-        int quantity = 3;
-        display(quantity);
-    }
 
-    public void decrement(View view) {
-        int quantity = 1;
-        display(quantity);
-        displayPrice(quantity - 1);
-    }
+
 
 
     /**
